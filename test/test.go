43e095af9fa6470031d03e8e51a385c76fccf898
bassolehermann <bassolehermann@gmail.com>
2019-06-08 00:45:29 +0000

    Isnegative

--- test/test.go
+++ test/test.go
@@ -1,6 +1,6 @@
 package main 
 
-import "fmt"
+import piscine ".."
 
 func IsNegative(nb int) {
 
@@ -13,7 +13,7 @@ func IsNegative(nb int) {
 
 func main() {
 
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
+	piscine.IsNegative(1)
+	piscine.IsNegative(0)
+	piscine.IsNegative(-1)
 }
