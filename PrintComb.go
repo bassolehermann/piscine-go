7288e7155f5913ec6d869b93e60a66cdcf6c49f7
bassolehermann <bassolehermann@gmail.com>
2019-06-10 20:25:03 +0000

    PrintComb

--- PrintComb.go
+++ PrintComb.go
@@ -1,29 +1,29 @@
 package piscine
+
 import "fmt"
 
 func PrintComb() {
 
-var n1, n2, n3
-  n1 = '0'
-  n2 = '0'
-  n3 = '0'
- 
-  for n1 <= '9' {
-    for n2 <= '9'  {
-      for n3 <= '9'  {
-        if n1 <= n2 && n2 <= n3  {
-        fmt.Println(0, &n1, 1);
-        fmt.Println(0, &n2, 1);
-        fmt.Println(0, &n3, 1);
-    	fmt.Println(0, "\n", 1);
-    }
-        n3 = n3 + 1;
-      }
-      n3 = '0';
-      n2 = n2 + 1;
-    }
-    n2 = '0';
-    n1 = n1 + 1;
-  }
+	var n1 = '0'
+	var n2 = '0'
+	var n3 = '0'
+
+	for n1 <= '9' {
+		for n2 <= '9' {
+			for n3 <= '9' {
+				if n1 <= n2 && n2 <= n3 {
+					fmt.Println(0, &n1, 1)
+					fmt.Println(0, &n2, 1)
+					fmt.Println(0, &n3, 1)
+					fmt.Println(0, "\n", 1)
+				}
+				n3 = n3 + 1
+			}
+			n3 = '0'
+			n2 = n2 + 1
+		}
+		n2 = '0'
+		n1 = n1 + 1
+	}
 
 }
