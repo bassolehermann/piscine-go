9fd0406d4d61835a22efe9f9434796bbde93f287
bassolehermann <bassolehermann@gmail.com>
2019-06-11 13:43:07 +0000

    PrintComb

---
+++ PrintComb.go
@@ -0,0 +1,16 @@
+package piscine
+
+import "fmt"
+func PrintComb() {
+
+
+var a,b,c int
+ 
+    for a = 0; a < 10; a++ {
+        for b = a + 1; b < 10; b++ {
+            for c = b + 1; c < 10; c++ {
+                fmt.Print(a,b,c,", ")
+            }
+        }
+    }
+}
