4adba2c054109baa38f3cc81d5406f908ef6cd7b
bassolehermann <bassolehermann@gmail.com>
2019-06-12 09:04:48 +0000

    PrintComb2

--- PrintComb2.go
+++ PrintComb2.go
@@ -7,9 +7,8 @@ func PrintComb2() {
 
 	for a = 0; a < 10; a++ {
 		for b = 0; b < 10; b++ {
-			for c = 0; c < 10; c++ {
+			for c = a; c < 10; c++ {
 				for d = b + 1; d < 10; d++ {
-
 					fmt.Print(a)
 					fmt.Print(b)
 					fmt.Print(" ")
@@ -18,28 +17,8 @@ func PrintComb2() {
 					fmt.Print(",")
 					fmt.Print(" ")
 
-					if a == 9 && b == 7 && c == 9 && d == 9 {
-
-						fmt.Print(9)
-						fmt.Print(8)
-						fmt.Print(" ")
-						fmt.Print(9)
-						fmt.Print(9)
-						break
-
-					}
-
-				}
-				if a == 9 && b == 7 && c == 9 && d == 9 {
-					break
 				}
 			}
-			if a == 9 && b == 7 && c == 9 && d == 9 {
-				break
-			}
-		}
-		if a == 9 && b == 7 && c == 9 && d == 9 {
-			break
 		}
 	}
 
