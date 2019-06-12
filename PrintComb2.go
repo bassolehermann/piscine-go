da9e985a2451e74d82e9d1a6b44dae0c0296800e
bassolehermann <bassolehermann@gmail.com>
2019-06-12 09:17:44 +0000

    PrintComb2

--- PrintComb2.go
+++ PrintComb2.go
@@ -9,13 +9,7 @@ func PrintComb2() {
 		for b = 0; b < 10; b++ {
 			for c = a; c < 10; c++ {
 				for d = b + 1; d < 10; d++ {
-					fmt.Print(a)
-					fmt.Print(b)
-					fmt.Print(" ")
-					fmt.Print(c)
-					fmt.Print(d)
-					fmt.Print(",")
-					fmt.Print(" ")
+
 					if a == 9 && b == 8 && c == 9 && d == 9 {
 
 						fmt.Print(a)
@@ -25,6 +19,14 @@ func PrintComb2() {
 						fmt.Print(d)
 						fmt.Print("\n")
 
+					} else {
+						fmt.Print(a)
+						fmt.Print(b)
+						fmt.Print(" ")
+						fmt.Print(c)
+						fmt.Print(d)
+						fmt.Print(",")
+						fmt.Print(" ")
 					}
 
 				}
