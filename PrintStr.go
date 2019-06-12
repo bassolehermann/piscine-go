9fb9044b4c947e5886f52a07d7abdad485acec59
bassolehermann <bassolehermann@gmail.com>
2019-06-12 20:39:38 +0000

    PrintStr

--- PrintStr.go
+++ PrintStr.go
@@ -2,11 +2,11 @@ package piscine
 
 import "fmt"
 
-func PrintStr(str string) {
+func Putstr(str string) {
 
 	for i := 0; i < len(str); i++ {
 		fmt.Printf(string(str[i]))
-
+		fmt.Print("%")
 	}
 
 }
