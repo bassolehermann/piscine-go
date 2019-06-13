39d666c95031bcd23c64f2741005e6c760802a63
bassolehermann <bassolehermann@gmail.com>
2019-06-13 20:24:40 +0000

    IterativeFactorial

--- IterativeFactorial.go
+++ IterativeFactorial.go
@@ -1,19 +1,23 @@
 package piscine
 
-import "math"
 
 func IterativeFactorial(nb int) int { 
 
 var factVal = 1 
-			
-			
-        	if nb < 0 || math.MaxInt  {
+const wordSize = 32 << (^uint(0) >> 63)
+const MinInt = -(1 << (wordSize - 1))
+const MaxInt = ^MinInt
+const MinUint = 0
+const MaxUint = MaxInt << 1 + 1	
+        	
+		
+        	for i:= 1; i<=nb; i++ {
+            		factVal *= (i)  
+        		}
+		if nb < 0 || factVal >MaxInt  {
         		return 0
         	}else{
-
-        	for i:= 1; i<=nb; i++ {
-            factVal *= (i)  
-        	}
+	
 				return factVal 
         	} 
 }
