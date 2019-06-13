ba6dd35e60078be7602a168c0d815bb0d9332a6c
bassolehermann <bassolehermann@gmail.com>
2019-06-13 16:25:44 +0000

    IterativeFactorial

--- IterativeFactorial.go
+++ IterativeFactorial.go
@@ -1,13 +1,18 @@
 package piscine
 
-func IterativeFactorial(nb int) int{
+func IterativeFactorial(nb int) uint64 { 
 
-fact:=1
-	for i:=1 ; i <= nb ; i++ {
-	
-	fact=fact*i
-	
-	
-	}
-return fact
+var factVal uint64 = 1 // uint64 is the set of all unsigned 64-bit integers.
+                       // Range: 0 through 18446744073709551615. 
+
+  
+    if(nb < 0){
+        return 0   
+    }else{        
+        for i:=1; i<=nb; i++ {
+            factVal *= uint64(i)  // mismatched types int64 and int
+        }
+         
+    }    
+    return factVal  /* return from function*/
 }
