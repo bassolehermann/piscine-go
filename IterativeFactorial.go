baec454f89aa3889549ac940942a15b504f9bd45
bassolehermann <bassolehermann@gmail.com>
2019-06-13 19:10:09 +0000

    IterativeFactorial

--- IterativeFactorial.go
+++ IterativeFactorial.go
@@ -1,8 +1,17 @@
 package piscine
-func IterativeFactorial(nb int) int {
-    if nb == 0 {
-        return 1
-    }
-    return nb * IterativeFactorial(nb-1)
-}
 
+
+func IterativeFactorial(nb int) int { 
+
+var factVal = 1 
+			for i:= 1; i<=nb; i++ {
+            factVal *= (i)  
+        	}
+
+        	if nb < 0  {
+        		return 0
+        	}
+        	else{
+				return factVal 
+        	} 
+}
