f87a26a8e36d4c6d220ada2b1ca72485c884a6f0
bassolehermann <bassolehermann@gmail.com>
2019-06-13 19:21:26 +0000

    IterativeFactorial

--- IterativeFactorial.go
+++ IterativeFactorial.go
@@ -1,12 +1,12 @@
 package piscine
 
-
+package "math"
 func IterativeFactorial(nb int) int { 
 
 var factVal = 1 
 			
-
-        	if nb < 0  {
+			
+        	if nb < 0 || math.Maxint  {
         		return 0
         	}else{
 
