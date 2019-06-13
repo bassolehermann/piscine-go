e7e870e27aefb2d4eea12a6f50959c847013a53d
bassolehermann <bassolehermann@gmail.com>
2019-06-13 16:29:48 +0000

    IterativeFactorial

--- IterativeFactorial.go
+++ IterativeFactorial.go
@@ -1,17 +1,8 @@
 
-func IterativeFactorial(nb int) uint64 { 
-
-var factVal uint64 = 1 // uint64 is the set of all unsigned 64-bit integers.
-                       // Range: 0 through 18446744073709551615. 
-
-  
-    if(nb < 0){
-        return 0   
-    }else{        
-        for i:=1; i<=nb; i++ {
-            factVal *= uint64(i)  // mismatched types int64 and int
-        }
-         
-    }    
-    return factVal  /* return from function*/
+func IterativeFactorial(nb int) int {
+    if nb == 0 {
+        return 1
+    }
+    return nb * IterativeFactorial(nb-1)
 }
+
