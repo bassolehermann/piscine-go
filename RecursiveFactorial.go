b6e6100e5535e7983b72d60df47ba9ff1088f8e7
bassolehermann <bassolehermann@gmail.com>
2019-06-14 10:21:33 +0000

    RecursiveFactorial

--- RecursiveFactorial.go
+++ RecursiveFactorial.go
@@ -4,6 +4,7 @@ package piscine
 
 func RecursiveFactorial(nb int ) int { 
 	var fact int 
+	var r int 
 const wordSize = 32 << (^uint(0) >> 63)
 const MinInt = -(1 << (wordSize - 1))
 const MaxInt = ^MinInt
@@ -19,7 +20,9 @@ const MaxUint = MaxInt << 1 + 1
 	
 	}
 
-	fact = nb * RecursiveFactorial(nb-1)
+	r = RecursiveFactorial(nb-1)
+	fact =nb*r
+
 	if nb<0 || fact > MaxInt {
 	 return 0
 	}
