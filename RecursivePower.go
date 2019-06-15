89c7c04ed668e570cfe9377957ff9b07d2b7ca59
bassolehermann <bassolehermann@gmail.com>
2019-06-15 19:50:38 +0000

    RecursivePower

--- RecursivePower.go
+++ RecursivePower.go
@@ -2,7 +2,7 @@ package piscine
 
 import "math"
 
-func IterativePower(nb int, power int) int {
+func RecursivePower(nb int, power int) int {
 	var result int
 
 	c := math.Pow(float64(nb), float64(power))
