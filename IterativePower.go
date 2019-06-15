0d0f68ccfa43e8c7c533ad1ceaf763fee6daf27e
bassolehermann <bassolehermann@gmail.com>
2019-06-15 13:57:28 +0000

    IterativePower

--- IterativePower.go
+++ IterativePower.go
@@ -1,21 +1,18 @@
 package piscine
 
-import "math"
-
-
 func IterativePower(nb int, power int ) int{ 
 
 var factVal int 
 
         	
 		
-        	for i:= 1; i<=nb; i++ {
-            		factVal= math.Pow(i,power)  
+        	for i:= 1; i<=power; i++ {
+            		nb=nb*i   
         		}
-		if nb < 0 && power <0 {
+		if power <0 {
         		return 0
         	}else{
 	
-				return factVal 
+				return nb 
         	} 
 }
