ca3d969de456512e871315df1a10b92346896e77
bassolehermann <bassolehermann@gmail.com>
2019-06-15 13:23:43 +0000

    RecursiveFactorial

--- RecursiveFactorial.go
+++ RecursiveFactorial.go
@@ -12,14 +12,13 @@ func RecursiveFactorial(nb int) int {
 var factVal int
 
 
-	if nb == 1 {
+	if nb == 0 {
 
 		return 1
 	}
 	
-	
-	
-	if  factVal > math.MaxInt32 {
+
+	if  nb < 0 || nb > math.MaxInt32 {
 
 		return 0
 
