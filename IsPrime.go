e9ebbcb8647cc5e225a7fcd8e52e0bbc739984ce
bassolehermann <bassolehermann@gmail.com>
2019-06-16 11:40:04 +0000

    IsPrime

--- IsPrime.go
+++ IsPrime.go
@@ -1,13 +1,26 @@
 package piscine
 
+import "math"
+
 func IsPrime(nb int) bool {
 
 	if nb%2 == 0 || nb%3 == 0 {
 
 		return false
 
-	} else {
-
-		return true
 	}
+	if float64(int(math.Sqrt(float64(nb)))) == math.Sqrt(float64(nb)) {
+
+		return false
+
+	}
+	for i := 3; i < int(math.Sqrt(float64(nb))); i = i + 2 {
+
+		if nb%i == 0 {
+			return false
+		}
+
+	}
+
+	return true
 }
