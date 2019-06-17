c9333b9bdaca346dfc0e8d77c725886bd87078bb
bassolehermann <bassolehermann@gmail.com>
2019-06-17 20:54:50 +0000

    IsNumeric

--- IsNumeric.go
+++ IsNumeric.go
@@ -2,11 +2,11 @@ package piscine
 
 import "strings"
 
-const alpha = "0123456789"
+const num = "0123456789"
 
 func IsNumeric(str string) bool {
    for _, char := range str {  
-      if !strings.Contains(alpha, strings.ToLower(string(char))) {
+      if !strings.Contains(num, strings.ToLower(string(char))) {
          return false
       }
    }
