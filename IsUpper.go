8141a40a315905ca86e5aea357f43c4f5d31f415
bassolehermann <bassolehermann@gmail.com>
2019-06-18 20:44:29 +0000

    IsUpper

--- IsUpper.go
+++ IsUpper.go
@@ -2,13 +2,13 @@ package piscine
 
 import "strings"
 
-const det ="abcdefghijklmnopqrstuvwxyz"
+const det ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func IsUpper(str string) bool {
    for _, char := range str {  
       if !strings.Contains(det, string(char)) {
-         return true
+         return false
       }
    }
-   return false
+   return true
 }
