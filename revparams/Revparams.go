9b93106ba3ce2b05d49eedb30de733dfdded6b80
bassolehermann <bassolehermann@gmail.com>
2019-06-18 18:17:29 +0000

    Revparams

--- revparams/Revparams.go
+++ revparams/Revparams.go
@@ -3,18 +3,17 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"os"
 )
 
-func reverse_words(s ) string {
-	words := strings.Fields(s)
-	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
-		words[i], words[j] = words[j], words[i]
-	}
-	return strings.Join(words, "\n")
-}
+
 
 func main() {
-	fmt.Println(reverse_words(os.Args))
+
+	
+	for i, j := 0, len(os.Args)-1; i < j; i, j = i+1, j-1 {
+		os.Args[i], os.Args[j] = os.Args[j], os.Args[i]
+	}
+
+	fmt.Println(os.Args)
 }
