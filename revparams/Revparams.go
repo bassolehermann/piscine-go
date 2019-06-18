d87270426b878b0f83cb25538ba01cd111dc11b0
bassolehermann <bassolehermann@gmail.com>
2019-06-18 18:05:52 +0000

    Revparams

--- revparams/Revparams.go
+++ revparams/Revparams.go
@@ -12,7 +12,7 @@ func reverse_words(s string) string {
 	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
 		words[i], words[j] = words[j], words[i]
 	}
-	return strings.Join(words, " ""\n")
+	return strings.Join(words, "\n")
 }
 
 func main() {
