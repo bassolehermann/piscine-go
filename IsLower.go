9500fd0639af16854800d959b35fd4bec2079159
bassolehermann <bassolehermann@gmail.com>
2019-06-18 07:37:06 +0000

    IsLower

--- IsLower.go
+++ IsLower.go
@@ -2,7 +2,7 @@ package piscine
 
 import "strings"
 
-const num =strings.ToUpper("abcdefghijklmnopqrstuvwxyz") 
+const num ="abcdefghijklmnopqrstuvwxyz"
 
 func IsLower(str string) bool {
    for _, char := range str {  
