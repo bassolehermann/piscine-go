8e768bd1defbbcff3260e9a01a10b31698eca6e1
bassolehermann <bassolehermann@gmail.com>
2019-06-19 11:07:40 +0000

    IsPrintable

--- IsPrintable.go
+++ IsPrintable.go
@@ -3,22 +3,19 @@ package piscine
 import "strings"
 import "strconv"
 
-const lettre = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-
 func IsPrintable(str string) bool {
-	
+const lettre = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
 var booleen bool 
 
 	for i := 0; i < len(str); i++ {
 
-		if !strings.Contains(lettre, strconv.QuoteToASCII (str)){
-
-			booleen=true
-
-		}else if strings.Contains(lettre, strconv.QuoteToASCII (str)){
+		if strings.Contains(lettre, strconv.QuoteToASCII (str)){
 
 			booleen=false
+			
+		}else if !strings.Contains(lettre, strconv.QuoteToASCII (str)) && !strings.Contains(strconv.QuoteToASCII (str), "\\"){
+
+			booleen=true
 		}
 		
 	}
