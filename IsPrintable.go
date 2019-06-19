de2cb9abafb1c673e8fa4b6064834a7bcd7d784f
bassolehermann <bassolehermann@gmail.com>
2019-06-19 10:18:35 +0000

    IsPrintable

--- IsPrintable.go
+++ IsPrintable.go
@@ -1,23 +1,31 @@
 package piscine
 
+import "strings"
 
-
-
-
+const lettre = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lettre1= "abcdefghijklmnopqrstuvwxyz"
 
 func IsPrintable(str string) bool {
+	var booleen bool
+
+	for _, char := range str {
+		if !strings.Contains(lettre, string(char)) {
+
+			booleen = true
+
+		} else if !strings.Contains(lettre1, string(char)) {
 
 
-	byteArray := []byte(str)
-	byteStr:=[]byte(str)
-
-	if byteArray>byteStr{
-
-		return false
-	}else{
-		return true
+		}else {
+			booleen = false
+		}
 	}
 
+	if booleen {
 
-	
+		return true
+	} else {
+
+		return false
+	}
 }
