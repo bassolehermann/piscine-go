dd5fc6ed4c4fe385e49614e360e8740d10b85926
bassolehermann <bassolehermann@gmail.com>
2019-06-20 20:08:31 +0000

    boolean

--- boolean/boolean.go
+++ boolean/boolean.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 import "os"
-import "strings"
 
 func printStr(str string) {
 	
@@ -21,8 +20,8 @@ func main() {
 
 	
 
-	if isEven(len(strings.Fiels(os.Args))){
-		
+	if isEven(len(os.Args)){
+
 		printStr("I have an odd number of arguments")
 	} else {
 		printStr("I have an even number of arguments")
