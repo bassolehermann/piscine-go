101cda111e3de56d4550c6be920fd4982c371506
bassolehermann <bassolehermann@gmail.com>
2019-06-21 09:49:41 +0000

    cat

---
+++ cat/cat.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+)
+
+func main() {
+
+	File,err := os.Open("quest8.txt")
+
+	if len(os.Args) == 0 {
+
+		fmt.Println(os.Args[0])
+	} else if err != nil {
+		fmt.Println(err.Error())
+	} else {
+
+		result, _ := ioutil.ReadAll(File)
+		fmt.Println(string(result))
+
+	}
+	
+
+}
