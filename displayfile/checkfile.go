afdb989c1bc1fc75468791ccaad069dc8f91dad0
bassolehermann <bassolehermann@gmail.com>
2019-06-21 08:47:18 +0000

    checkfile

--- displayfile/checkfile.go
+++ displayfile/checkfile.go
@@ -12,12 +12,13 @@ func main() {
 	if os.Args == nil {
 		fmt.Println(err)
 	} else if len(os.Args) == 1 {
-		fmt.Println("Too many arguments")
-	} else {
+		
 		arg := make([]byte, 31)
 		file.Read(arg)
 		fmt.Println(string(arg))
 		file.Close()
+	} else {
+		fmt.Println("Too many arguments")
 	}
 
 }
