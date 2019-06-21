eb29cec4b4caf14cbde9092c19b28c9bb97f1417
bassolehermann <bassolehermann@gmail.com>
2019-06-21 23:30:10 +0000

    Any

---
+++ Any.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Any(f func(string) bool, arr []string) bool {
+	var boo bool
+	for _, val := range arr {
+
+		if f(val) {
+			boo = true
+		} else {
+			boo = false
+		}
+
+	}
+	if boo {
+		return true
+	} else {
+		return false
+	}
+}
+func verif(str string) {
+
+}
