9f9144d496fcdb10cae40cbe34f6f39d3edbc16c
bassolehermann <bassolehermann@gmail.com>
2019-06-21 23:11:13 +0000

    Map

--- Map.go
+++ Map.go
@@ -1,7 +1,8 @@
 package piscine
 
 func Map(f func(int) bool, arr []int) []bool {
-	var booleen []bool
+	var booleen = make([]bool, len(arr), len(arr))
+
 	for i, val := range arr {
 
 		booleen[i] = f(val)
