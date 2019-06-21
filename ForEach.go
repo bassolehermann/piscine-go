973b5a5a9de6cdabad6351600501b0c07d20fe7c
bassolehermann <bassolehermann@gmail.com>
2019-06-21 15:54:06 +0000

    ForEach

---
+++ ForEach.go
@@ -0,0 +1,20 @@
+
+
+package main
+
+
+
+func ForEach(f func(int)int, arr []int) []int {
+var a = make([]int, len(arr), len(arr))
+    for index, val := range arr{
+
+        a[index] = f(val)
+    }
+    return a
+}
+
+func PutNbr (nbr int) int {
+
+	return nbr
+
+}
