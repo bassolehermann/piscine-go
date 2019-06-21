dc6fd39288c2882268be301a996888273cd28557
bassolehermann <bassolehermann@gmail.com>
2019-06-21 09:22:03 +0000

    checkfile

--- displayfile/checkfile.go
+++ displayfile/checkfile.go
@@ -7,13 +7,16 @@ import (
 
 func main() {
 
-	//file, err := os.Open("quest8.txt")
+	file, _ := os.Open("quest8.txt")
 
 	if len(os.Args) == 1 {
-		fmt.Println("file name missing")
+		fmt.Println("File name missing")
 	} else if len(os.Args) == 2 {
 		
-		fmt.Println("file exist")
+		tab:=make([]byte ,14)
+		file.Read(tab)
+		fmt.Println(string(tab))
+
 	} else if len(os.Args) >2 {
 		fmt.Println("Too many arguments")
 	}
