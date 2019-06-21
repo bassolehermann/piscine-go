9a40188713cc28eaabc952f76854095369d65092
bassolehermann <bassolehermann@gmail.com>
2019-06-21 10:39:04 +0000

    git add cat.go

--- cat/cat.go
+++ cat/cat.go
@@ -10,10 +10,7 @@ func main() {
 
 	File,err := os.Open("quest8.txt")
 
-	if len(os.Args) == 0 {
-
-		fmt.Println(os.Args[0])
-	} else if err != nil {
+ if err != nil {
 		fmt.Println(err.Error())
 	} else if os.Args[1]=="quest8.txt" || os.Args[1]=="quest8T.txt" {
 
