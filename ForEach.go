520ecc5482b3e2cbf34b65184db11e4c146e7520
bassolehermann <bassolehermann@gmail.com>
2019-06-21 16:46:49 +0000

    ForEach

--- ForEach.go
+++ ForEach.go
@@ -2,13 +2,15 @@
 
 package piscine
 
-
+import "fmt"
 
 func ForEach(f func(int)int, arr []int) []int {
 var a = make([]int, len(arr), len(arr))
     for i, val := range arr{
 
         a[i] = f(val)
+	fmt.Print(a[i])
+	
     }
     return a
 }
