504cbd4584e8b14893211591b24f45846d211e64
bassolehermann <bassolehermann@gmail.com>
2019-06-21 22:55:42 +0000

    Map

---
+++ Map.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Map(f func(int) bool, arr []int) []bool {
+	var booleen []bool
+	for i, val := range arr {
+
+		booleen[i] = f(val)
+
+	}
+
+	return booleen
+}
+
+func IsPrime(nb int) {
+
+}
