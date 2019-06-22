421d4384c49ca857ae7b1892384d09dc155f936c
bassolehermann <bassolehermann@gmail.com>
2019-06-22 16:48:56 +0000

    IsSorted

---
+++ IsSorted.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "sort"
+
+func IsSorted(f func(a, b int) int, tab []int) bool {
+
+		if tab==sort.Ints(tab) {
+			return true
+		}else {
+			return false
+		}
+
+
+}
+
+
+func f(nbr1,nbr2 int) int {
+
+	if nbr1> nbr2{
+
+		return nbr1
+	} else if nbr1==nbr2{
+
+		return 0
+	}else {
+
+	
+		return -res
+	}
+}
