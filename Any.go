49a5fec1608f174cdd1a13831e484dd95dff598b
bassolehermann <bassolehermann@gmail.com>
2019-06-22 00:45:29 +0000

    Any

--- Any.go
+++ Any.go
@@ -6,7 +6,7 @@ func Any(f func(string) bool, arr []string) bool {
 	var boo bool
 	for _, val := range arr {
 
-		if f(val) && len(val) == 1 {
+		if f(val) || len(val) == 1 {
 			boo = true
 		} else {
 			boo = false
