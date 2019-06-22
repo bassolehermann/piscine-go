49d70016a3dadb49c5e6456facd10c08461bfbdf
bassolehermann <bassolehermann@gmail.com>
2019-06-22 10:02:58 +0000

    Any

--- Any.go
+++ Any.go
@@ -6,7 +6,7 @@ func Any(f func(string) bool, arr []string) bool {
 	var boo bool
 	for _, val := range arr {
 
-		if f(val) &&  strings.Contains(f(val), nbr) {
+		if f(val) &&  strings.Contains(val, "4") {
 			boo = true
 		} else {
 			boo = false
