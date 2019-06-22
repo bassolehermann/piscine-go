1ee6152fe3bbb916fb0993e3a5832391fc9ee1bf
bassolehermann <bassolehermann@gmail.com>
2019-06-22 00:44:03 +0000

    Any

--- Any.go
+++ Any.go
@@ -1,14 +1,12 @@
 package piscine
 
-import "strings"
-
 const nbr = "0123456789"
 
 func Any(f func(string) bool, arr []string) bool {
 	var boo bool
 	for _, val := range arr {
 
-		if f(val) && strings.Contains(nbr, val) {
+		if f(val) && len(val) == 1 {
 			boo = true
 		} else {
 			boo = false
