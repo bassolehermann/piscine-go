7eb7e8ef53a4f95a40baf8fa5e4688178a8db374
bassolehermann <bassolehermann@gmail.com>
2019-06-22 11:17:41 +0000

    CountIf

--- CountIf.go
+++ CountIf.go
@@ -10,8 +10,8 @@ func CountIf(f func(string) bool, tab []string) int {
 		if f(val) {
 			boo = true
 			nombre=i+1
-		} else if f(val) {
-			boo = true
+		} else {
+			boo = false
 			nombre =(len(val)-i)
 		}
 
