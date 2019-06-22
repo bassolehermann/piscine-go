42debd2ad8627d3465f45f8708f4dace609ff984
bassolehermann <bassolehermann@gmail.com>
2019-06-22 19:08:38 +0000

    IsSorted

--- IsSorted.go
+++ IsSorted.go
@@ -13,8 +13,10 @@ var booleen bool
 				booleen=true
 			}else if f(tab[i],tab[i+1])==0{
 				booleen=true
+			}else if f(tab[i],tab[i+1])<0{
+				booleen=true
 			}else{
-				booleen=false
+				booleen=true
 			}
 		}
 
