8f8f6bc77e6de637fd18ad79b43fce20d0200679
bassolehermann <bassolehermann@gmail.com>
2019-06-23 16:45:43 +0000

    IsSorted

--- IsSorted.go
+++ IsSorted.go
@@ -10,6 +10,9 @@ func IsSorted(f func(a, b int) int, tab []int) bool {
 			booleen = true
 		} else if f(tab[i], tab[i+1]) < 0 {
 			booleen = true
+		} else if tab[i] == 0 && tab[i+1] == 0 {
+
+			booleen = true
 		}
 	}
 
