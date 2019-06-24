38546f6ef3545b657ae2d99b9d81f15223f7cafa
bassolehermann <bassolehermann@gmail.com>
2019-06-24 10:44:04 +0000

    Abort

--- Abort.go
+++ Abort.go
@@ -4,11 +4,11 @@ package piscine
 func Abort(a, b, c, d, e int) int {
 var result int
 
-	x:=[]int={a,b,c,d,e}
+	x:=[]int{a,b,c,d,e}
 
-	int.Sort(x)
+	sort.Ints(x)
 
-	result = x[1] / x[3]
+	result = (x[1] + x[3])/2
 
 	return result
 
