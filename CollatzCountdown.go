2e9587dc235f1b694bcefdc7a37c7f8949a17aa0
bassolehermann <bassolehermann@gmail.com>
2019-06-24 12:18:25 +0000

    CollatzCountdown

--- CollatzCountdown.go
+++ CollatzCountdown.go
@@ -2,18 +2,18 @@ package piscine
 
 
 func CollatzCountdown(start int) int {
-var result=1
+
 	for i := start; i > 1; i-- {
 	
 			if start%2==0{
 				start=start/2
-
+		
 			}else{
 				start=start*3+1
 			}
 
-			result++
+			
 	}
 
-return result
+return start
 }
