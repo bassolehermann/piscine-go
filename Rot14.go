017ae8027b2c7f93e5eaee9e988729b2984c506d
bassolehermann <bassolehermann@gmail.com>
2019-06-24 09:13:56 +0000

    Rot14

--- Rot14.go
+++ Rot14.go
@@ -2,11 +2,11 @@ package piscine
 
 func Rot14(str string) string {
 
-	x:=[]byte(string)
+	x:=[]byte(str)
 
     input := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
     output := []byte("OPQRSTUVWXYZABCDEFGHIJKLMNopqrstuvwxyzabcdefghijklmn")
-    match := bytes.Index(input, []byte{x})
+    match := bytes.Index(input, x)
     if match == -1 {
         return x
     }
