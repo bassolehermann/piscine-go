76a4cd6a79109ec6909ed26f33352fe3ddfdf7c4
Crashman <48332956+bassolehermann@users.noreply.github.com>
2019-06-24 14:27:48 +0000

    Update comcheck.go

--- comcheck/comcheck.go
+++ comcheck/comcheck.go
@@ -5,11 +5,6 @@ import "strings"
 
 func main(){
 	
-	if os.Args[i]=="galaxy01"{
-			fmt.Println("")
-			break
-		}
-
 	for i := 0; i < len(os.Args); i++ {
 
 		if strings.Contains(os.Args[i],"01") || strings.Contains(os.Args[i],"galaxy")   {
