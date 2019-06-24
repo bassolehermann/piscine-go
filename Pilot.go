8182a19f67df53f672e9f5ade450d10bae2f0c09
bassolehermann <bassolehermann@gmail.com>
2019-06-24 16:59:14 +0000

    Pilot

--- Pilot.go
+++ Pilot.go
@@ -1,8 +1,23 @@
-package student
+package main
 
 type donnie struct {
 	Name string
 	Life  float64
 	Age int
 	Aircraft
+}
+
+import (
+	"fmt"
+	student ".."
+)
+
+func main() {
+	var donnie student.Pilot
+	donnie.Name = "Donnie"
+	donnie.Life = 100.0
+	donnie.Age = 24
+	donnie.Aircraft = student.AIRCRAFT1
+
+	fmt.Println(donnie)
 }
