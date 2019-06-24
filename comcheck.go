c508bf700e9923b6cc22fe30eabb9c598912bcc1
bassolehermann <bassolehermann@gmail.com>
2019-06-24 16:38:32 +0000

    comcheck.go

--- comcheck.go
+++ comcheck.go
@@ -1,15 +1,15 @@
 package main 
 import "os"
 import "fmt"
-import "string"
+
 
 func main(){
-
-
+	
 	for i := 0; i < len(os.Args); i++ {
 
-		if Strings.Contains(os.Args[i],"01") || Strings.Contains(os.Args[i],"galaxy") || Strings.Contains(os.Args[i],"galaxy 01")  {
+		if os.Args[i]=="01" || os.Args[i]=="galaxy" || os.Args[i]=="galaxy 01"  {
 			fmt.Println("Alert!!!")
+			break
 		}
 		
 	}
