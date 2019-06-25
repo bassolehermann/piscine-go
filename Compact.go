742ad177a8dbaab58e713b06b3352007ff561520
bassolehermann <bassolehermann@gmail.com>
2019-06-25 12:27:41 +0000

    Compact

---
+++ Compact.go
@@ -0,0 +1,18 @@
+
+import "strings"
+func Compact(ptr *[]string, length int) int {
+
+tab=make([]string, length)	
+
+for i := 0; i < length; i++ {
+
+	if ptr[i] != nil {
+		tab[i]=ptr[i]
+	}
+	
+}
+
+return len(tab)
+}
+
+
