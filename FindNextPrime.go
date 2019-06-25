97d6725e8b5d02d708b0dc5c6528df4bf5b4d87c
bassolehermann <bassolehermann@gmail.com>
2019-06-25 18:29:53 +0000

    FindNextPrime

--- FindNextPrime.go
+++ FindNextPrime.go
@@ -11,10 +11,14 @@ func FindNextPrime(nb int) int {
 	if nb<0{
 		return 2
 	}
-	if nb%2 == 0 || nb%3 == 0 {
-
-		
-
+	if nb<7{
+		return 7
+	}
+	if nb<11{
+		return 11
+	}
+	if nb<13{
+		return 13
 	}
 	if float64(int(math.Sqrt(float64(nb)))) == math.Sqrt(float64(nb)) {
 
