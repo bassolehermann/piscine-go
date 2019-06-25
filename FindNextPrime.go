1be945374d776eaf803f41375d61c426270b641b
bassolehermann <bassolehermann@gmail.com>
2019-06-25 17:55:10 +0000

    FindNextPrime

---
+++ FindNextPrime.go
@@ -0,0 +1,34 @@
+package piscine
+
+import "math"
+
+func FindNextPrime(nb int) int {
+
+	if nb == 2 || nb == 3 {
+
+		return nb
+	}
+
+	if nb%2 == 0 || nb%3 == 0 {
+
+		
+
+	}
+	if float64(int(math.Sqrt(float64(nb)))) == math.Sqrt(float64(nb)) {
+
+		return false
+
+	}
+	for i := 3; i < int(math.Sqrt(float64(nb))); i = i + 2 {
+
+		if nb%i == 0 {
+			return false
+		}else {
+			return i
+			break
+		}
+
+	}
+
+	return nb
+}
