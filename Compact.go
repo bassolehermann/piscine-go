155acd838484ac68890c7e8bc5d4b13e26c1274f
bassolehermann <bassolehermann@gmail.com>
2019-06-25 13:36:12 +0000

    Compact

--- Compact.go
+++ Compact.go
@@ -1,13 +1,15 @@
 package piscine
-import "strings"
+
 func Compact(ptr *[]string, length int) int {
 
-tab=make([]string, length)	
+
+tab:=make([]string, length)	
 
 for i := 0; i < length; i++ {
 
-	if ptr[i] != nil {
-		tab[i]=ptr[i]
+	if (*ptr)[i] != " " {
+		tab[i]=(*ptr)[i]
+
 	}
 	
 }
