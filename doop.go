12c36eb990fc2117c923f0189855155526dc017e
bassolehermann <bassolehermann@gmail.com>
2019-06-29 18:06:54 +0000

    doop

---
+++ doop.go
@@ -0,0 +1,54 @@
+package main 
+
+
+import "fmt"
+
+
+func main(){
+var result int
+
+ if len(os.Args)==3 {
+
+			if os.Args[1]=="+" {
+
+			result=strconv.Atoi(os.Args[0])+ strconv.Atoi(os.Args[2])
+			fmt.Print(result)
+
+			}else if os.Args[1]=="-" {
+
+				result=strconv.Atoi(os.Args[0])- strconv.Atoi(os.Args[2])
+				fmt.Print(result)
+	
+				}else if os.Args[1]=="*" {
+
+				result=strconv.Atoi(os.Args[0])* strconv.Atoi(os.Args[2])
+				fmt.Print(result)
+			
+			}else if os.Args[1]=="/" {
+				if strconv.Atoi(os.Args[2])==0{
+
+					fmt.Println("No division by 0")
+
+				}else{
+					result=strconv.Atoi(os.Args[0])/ strconv.Atoi(os.Args[2])
+					fmt.Print(result)
+				}
+			}else if os.Args[1]=="%"{
+				if strconv.Atoi(os.Args[2])==0{
+
+					fmt.Println("No modulo by 0")
+
+				}else{
+					result=strconv.Atoi(os.Args[0])% strconv.Atoi(os.Args[2])
+					fmt.Print(result)
+				}
+			}else{
+				fmt.Print(0)
+			}
+
+
+
+ }
+	
+
+	
